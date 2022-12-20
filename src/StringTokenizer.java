0c4d203211df77b5a55f13e5c41328bce80b1d02
Krrish Verma <krrishverma06@gmail.com>
2022-12-19 20:52:56 -0600

    Final Commit

--- src/StringTokenizer.java
+++ src/StringTokenizer.java
@@ -1,16 +1,13 @@
 import java.io.*;
 import java.util.*;
 
-public class Soylent {
+public class StringTokenizer {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < n; i++){
-            pw.println( (int) Math.ceil(Integer.parseInt(br.readLine())/400.0));
-        }
 
         br.close();
         pw.close();
