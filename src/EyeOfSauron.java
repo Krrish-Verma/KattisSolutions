77ba4bdfc2beaaacf7a5547e85907577ee92dc16
Krrish Verma <krrishverma06@gmail.com>
2022-12-23 15:59:07 -0600

    Final Commit

--- src/EyeOfSauron.java
+++ src/EyeOfSauron.java
@@ -2,14 +2,17 @@ import java.util.*;
 
 public class EyeOfSauron {
     public static void main(String[] args) {
+
+
         Scanner sc = new Scanner(System.in);
 
         String str = sc.nextLine();
+        int n = (str.length()/2)-1;
 
-        int length = str.length();
-        
-        int middle = length/2;
+        if (str.length() % 2 != 0) System.out.println("fix");
 
-        String substring = str.substring(length);
+        else if (str.split("")[n].equals("(")) System.out.println("correct");
+
+        else System.out.println("fix");
     }
 }
