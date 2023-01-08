64519008bab2ccae6c00236b34d38d2b5ce9d743
Krrish Verma <krrishverma06@gmail.com>
2023-01-08 15:51:25 -0600

    Final Commit

---
+++ src/YinYang.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class YinYang {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        int w = 0, k = 0;
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == 'W') w++;
+            else k++;
+        }
+        if(k == w) pw.println(1);
+        else pw.println(0);
+
+        br.close();
+        pw.close();
+    }
+
+}
